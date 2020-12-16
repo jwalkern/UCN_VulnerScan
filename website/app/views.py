8b24c2e4b8af9f0d9887495db16652fdc8dc5a48
jwalkern <56714873+jwalkern@users.noreply.github.com>
2020-12-16 19:43:49 +0100

    123

--- website/app/views.py
+++ website/app/views.py
@@ -2,7 +2,7 @@
 
 from flask import render_template
 from app import app
-from vulnerscan.nmap.xml_driver import xml_read
+from vulnerScan.nmap.xml_driver import xml_read
 
 import io
 import base64
