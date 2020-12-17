77febdcff9b2403d4565bc21fb1221c63d770eda
jwalkern <56714873+jwalkern@users.noreply.github.com>
2020-12-17 09:35:08 +0100

    123

--- website/app/views.py
+++ website/app/views.py
@@ -50,14 +50,12 @@ def sojle():
     labels = ['G1', 'G2', 'G3', 'G4', 'G5']
     men_means = [20, 35, 30, 35, 27]
     women_means = [25, 32, 34, 20, 25]
-    men_std = [2, 3, 4, 1, 2]
-    women_std = [3, 5, 2, 3, 3]
     width = 0.35       # the width of the bars: can also be len(x) sequence
     
     fig, ax = plt.subplots()
     
-    ax.bar(labels, men_means, width, yerr=men_std, label='Men')
-    ax.bar(labels, women_means, width, yerr=women_std, bottom=men_means,
+    ax.bar(labels, men_means, width, label='Men')
+    ax.bar(labels, women_means, width, bottom=men_means,
            label='Women')
     
     ax.set_ylabel('Scores')
