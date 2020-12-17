930fe454bdeda1edf8267089ea10d4847a08bfc5
jwalkern <56714873+jwalkern@users.noreply.github.com>
2020-12-17 10:34:34 +0100

    123

--- website/app/views.py
+++ website/app/views.py
@@ -22,13 +22,23 @@ def about():
 
 @app.route("/myplot", methods=['GET'])
 def plotView():
-    fig = Figure()
-    axis = fig.add_subplot(1, 1, 1)
-    axis.set_title("title")
-    axis.set_xlabel("x-axis")
-    axis.set_ylabel("y-axis")
-    axis.grid()
-    axis.plot(range(5), range(5), "ro-")
+     # Generate plot
+    port = ['P21', 'P22', 'P80', 'P139', '145']
+    men_means = [20, 35, 30, 35, 27]
+    women_means = [25, 32, 34, 20, 25]
+    men_std = [2, 3, 4, 1, 2]
+    women_std = [3, 5, 2, 3, 3]
+    width = 0.35       # the width of the bars: can also be len(x) sequence
+    
+    fig, ax = plt.subplots()
+    
+    ax.bar(port, men_means, width, yerr=men_std, label='Active Exploits')
+    ax.bar(port, women_means, width, yerr=women_std, bottom=men_means,
+           label='Non active Exploits')
+    
+    ax.set_ylabel('Exploits')
+    ax.set_title('Ip-Adresse')
+    ax.legend()
     
     # Convert plot to PNG image
     pngImage = io.BytesIO()
@@ -45,25 +55,5 @@ def nmap_result():
     host = x.xml_read('text.xml')
     pass
 
-@app.route('/test')
-def sojle():
-    labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-    men_means = [20, 35, 30, 35, 27]
-    women_means = [25, 32, 34, 20, 25]
-    width = 0.35       # the width of the bars: can also be len(x) sequence
-    
-    fig, ax = plt.subplots()
-    
-    ax.bar(labels, men_means, width, label='Men')
-    ax.bar(labels, women_means, width, bottom=men_means,
-           label='Women')
-    
-    ax.set_ylabel('Scores')
-    ax.set_title('Scores by group and gender')
-    ax.legend()
-    
-    plt.show()
-    
-    return render_template("test.html", graf=plt.show())
     
     
