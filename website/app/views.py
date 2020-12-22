ae17b06fdf13784c186bd81837c5423e2a043f55
jwalkern <56714873+jwalkern@users.noreply.github.com>
2020-12-22 09:52:20 +0100

    Signed-off-by: jwalkern <56714873+jwalkern@users.noreply.github.com>

--- website/app/views.py
+++ website/app/views.py
@@ -5,6 +5,7 @@ from app import app
 import io
 import base64
 import matplotlib.pyplot as plt
+from app import xml_driver
 
 
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
@@ -46,5 +47,67 @@ def plotView():
     
     return render_template('image.html', image=pngImageB64String)
 
+ 
+@app.route("/result", methods=["GET"])
+def resultView():
+    xmlFile = 'test.xml'
+    hosts = xml_driver(xmlFile)
+    counter = 0
+    for host in hosts:
     
+        counter += 1
+        filePath = f"./static/images/plot{counter}.png"
+        title = host['address']
+        
+        chartPorts = []
+        verifiedExploits = []
+        possibleExploits = []
+        
+        if 'ports' in host:
+            for port in host['ports']:
+                
+                chartPorts.append(port['port'])
+                if 'active exploits:' in port:
+                    verifiedExploits.append(port['active exploits:'])
+                else:
+                    verifiedExploits.append(0)
+                    
+                if 'exploits:' in port:
+                    possibleExploits.append(port['exploits:'])
+                else:
+                    possibleExploits.append(0)
+ 
+        
+                # Generate plot
+                port = chartPorts
+                vExploits = verifiedExploits
+                pExploits = possibleExploits
+                # the width of the bars: can also be len(x) sequence
+                
+                fig, ax = plt.subplots()
+                width = 0.35   
+                ax.bar(port, vExploits, width, label='Not confirmed exploits')
+                ax.bar(port, pExploits, width, bottom=vExploits,
+                       label='Exploits')
+                
+                ax.set_ylabel('Exploits')
+                ax.set_title(title)
+                
+                ax.legend()
+                
+                
+                
+                # # Convert plot to PNG image
+                # pngImage = io.BytesIO()
+                # FigureCanvas(fig).print_png(pngImage)
+                
+                # # Encode PNG image to base64 string
+                # pngImageB64String = "data:image/png;base64,"
+                # pngImageB64String += base64.b64encode(pngImage.getvalue()).decode('utf8')
+               
+        plt.savefig(filePath)
+        plt.clf()
+ 
+ 
     
+    return render_template("image.html")
