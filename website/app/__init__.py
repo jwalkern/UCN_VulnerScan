36c1109d2962f451d69bf135b4a309f9e6f6c837
jwalkern <56714873+jwalkern@users.noreply.github.com>
2020-12-28 09:53:33 +0100

    Signed-off-by: jwalkern <56714873+jwalkern@users.noreply.github.com>

--- website/app/__init__.py
+++ website/app/__init__.py
@@ -2,7 +2,7 @@
 
 from flask import Flask
 
-app = Flask(__name__, instance_relative_config=True)
+app = Flask(__name__, instance_relative_config=True, static_folder='/home/pi/vulnerScan/website/app/static')
 
 from app import views
 
